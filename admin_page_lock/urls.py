080e43a0c2e2c40dde17659cf1ca424f407dd1f9
Thomas Vanesse <thomas@symbiose.bio>
2023-10-18 17:29:44 +0200

    Switched from url() to path() in the routes definition

--- admin_page_lock/urls.py
+++ admin_page_lock/urls.py
@@ -1,23 +1,22 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
+from django.urls import path
 
 from admin_page_lock.views import ClosePageConnection, GetPageInfo, OpenPageConnection
 
 urlpatterns = [
     # Close Page Connection.
-    url(
-        r"^close_page_connection/$",
+    path(
+        "close_page_connection/",
         ClosePageConnection.as_view(),
         name="page_lock_close_page_connection",
     ),
     # Get Page Info.
-    url(
-        r"^get_page_info/$", GetPageInfo.as_view(), name="page_lock_get_page_connection"
-    ),
+    path("get_page_info/", GetPageInfo.as_view(), name="page_lock_get_page_connection"),
     # Open Page Connection.
-    url(
-        r"^open_page_connection/$",
+    path(
+        "open_page_connection/",
         OpenPageConnection.as_view(),
         name="page_lock_open_page_connection",
     ),
