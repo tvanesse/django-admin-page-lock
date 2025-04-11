7b61cd6c704071cc926e0315a8ad05a6c977f298
Thomas Vanesse <thomas@symbiose.bio>
2025-04-11 15:30:49 +0200

    Update urlpatterns to remove regex and use simpler path syntax

--- admin_page_lock/urls.py
+++ admin_page_lock/urls.py
@@ -7,17 +7,15 @@ from admin_page_lock.views import ClosePageConnection, GetPageInfo, OpenPageConn
 urlpatterns = [
     # Close Page Connection.
     path(
-        r"^close_page_connection/$",
+        "close_page_connection/",
         ClosePageConnection.as_view(),
         name="page_lock_close_page_connection",
     ),
     # Get Page Info.
-    path(
-        r"^get_page_info/$", GetPageInfo.as_view(), name="page_lock_get_page_connection"
-    ),
+    path("get_page_info/", GetPageInfo.as_view(), name="page_lock_get_page_connection"),
     # Open Page Connection.
     path(
-        r"^open_page_connection/$",
+        "open_page_connection/",
         OpenPageConnection.as_view(),
         name="page_lock_open_page_connection",
     ),
